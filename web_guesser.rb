2c7086a3617fe4d0d9be8eb94afc79eaa9d1126a
Laura <Laura.Stephens@utexas.edu>
2017-02-09 18:54:56 -0600

    added background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,9 +8,10 @@ NUM = rand(0..100)
 get '/' do 
 
 	guess = params['guess'].to_i
-	message = check_guess(guess)
+	message = check_guess(guess)[0]
+	background = check_guess(guess)[1]
 	erb :index, :locals => {:num => NUM, 
-		:message => message}
+		:message => message, :background => background}
 	#throw params.inspect
 end
 
@@ -18,15 +19,20 @@ def check_guess(guess)
 	#puts guess-NUM
 	if guess ==NUM
 		message = "You got it right!"
+		background = "background: green"
 	elsif guess-NUM > 5
 		message = "Way too high!"
+		background = "background: red"
 	elsif guess-NUM < -5
 		message = "Way too low!"
+		background = "background: red"
 	elsif guess-NUM >0
 		message = "Too high!"
+		background = "background: pink"
 	elsif guess-NUM <0
 		message = "Too low!"
+		background = "background: pink"
 	end
-	message
+	[message,background]
 end
 
